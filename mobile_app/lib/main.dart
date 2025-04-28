d3ce4e7ed73159fd17401227df2795f8f3f76389
Binod Thapa <binod.thapachhetry@gmail.com>
2025-04-28 00:27:00 -0400

    feat: initialize Firebase in main.dart for mobile app

--- mobile_app/lib/main.dart
+++ mobile_app/lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'ble_permission_gate.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
   runApp(const MobileHealthApp());
 }
 
